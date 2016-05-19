dcab7758c09ad76a81bcc4eb5a0edc87e19bb0a2
Samuel Dillow <tarwich@gmail.com>
2016-05-19 13:29:23 -0500

    Trying to make the message prettier
    Attempt #1

--- README.md
+++ README.md
@@ -1,35 +1,38 @@
 # slack-post
 
-Posts wercker build/deploy status to a [Slack Channel](https://slack.com/).
+Posts wercker build/deploy status to a [Slack] channel.
 
-[![wercker status](https://app.wercker.com/status/49d7c3919df2d65ed4679bcc86eb3477/m "wercker status")](https://app.wercker.com/project/bykey/49d7c3919df2d65ed4679bcc86eb3477)
+This project is forked from the awesome work of [kobim]
+
+[Slack]: https://slack.com/
+[kobim]: https://github.com/kobim/wercker-step-slack-post
+
+<!-- [![wercker status](https://app.wercker.com/status/49d7c3919df2d65ed4679bcc86eb3477/m "wercker status")](https://app.wercker.com/project/bykey/49d7c3919df2d65ed4679bcc86eb3477) -->
 
 ### Required fields
 
-* `url` - Your incoming webhook url (get it at `https://<subdomain>.slack.com/services/new/incoming-webhook`).
+* `url` - Your incoming webhook url (get it at https://slack.com/services/new/incoming-webhook).
 
 ### Optional fields
 
 * `channel` - The Slack Channel you wish to post to (default - `#general`).
-* `username` - The robot's username (default - `wreckerbot`).
+* `username` - The robot's username (default - `werckerbot`).
 
 ### How to configure?
 
-Under your project settings, add new Pipeline variable named `SLACK_URL` with your
-WebHook url. You then reference this variable in your [wercker.yml](http://devcenter.wercker.com/articles/werckeryml/).
+Under your project settings, add new Pipeline variable named `SLACK_URL` with your WebHook url. You then reference this variable in your [wercker.yml](http://devcenter.wercker.com/articles/werckeryml/).
 
 
 # Example
 
 Add `SLACK_URL` as deploy target or application environment variable.
 
-
-    build:
-        after-steps:
-            - kobim/slack-post:
-                url: $SLACK_URL
-                channel: dev # OPTIONAL
-                username: builder # OPTIONAL
+  build:
+    after-steps:
+      - tarwich/slack-post:
+        url:      $SLACK_URL
+        channel:  dev        # OPTIONAL
+        username: builder    # OPTIONAL
 
 # License
 
@@ -53,24 +56,3 @@ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-# Changelog
-
-## 0.0.6
-- Bugfix
-
-## 0.0.5
-- New slack format
-
-## 0.0.4
-- Removed the owner name from the post text.
-
-## 0.0.3
-- Updated 'fatal' directive to 'fail'
-
-## 0.0.2
-- Better README
-- Added repository name in the message itself.
-
-## 0.0.1
-- initial version
--- run.sh
+++ run.sh
@@ -29,11 +29,61 @@ fi
 
 if [[ $WERCKER_GIT_DOMAIN == bitbucket* ]]; then export GIT_TREE="commits"; else export GIT_TREE="commit"; fi
 
+# Get the time of the build
+WERCKER_TIME_START=$WERCKER_MAIN_PIPELINE_STARTED
+WERCKER_TIME_END=$(date +"%s")
+WERCKER_TIME_DIFF=$(($WERCKER_TIME_END-$WERCKER_TIME_START))
+WERCKER_TIME_SPENT="in $(($WERCKER_TIME_DIFF / 60)) min $(($WERCKER_TIME_DIFF % 60)) sec."
+
+export LATEST_COMMIT=$(git log -1 --pretty=%B)
 export WERCKER_SLACK_POST_GIT="<http://$WERCKER_GIT_DOMAIN/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/$GIT_TREE/$WERCKER_GIT_COMMIT|$WERCKER_GIT_REPOSITORY/$WERCKER_GIT_BRANCH@${WERCKER_GIT_COMMIT:0:3}..>"
+export WERCKER_SLACK_POST_GIT="<http://$WERCKER_GIT_DOMAIN/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/$GIT_TREE/$WERCKER_GIT_COMMIT|$LATEST_COMMIT>"
 
-export WERCKER_SLACK_POST_MESSAGE="<$WERCKER_APPLICATION_URL|$WERCKER_APPLICATION_NAME> $WERCKER_SLACK_POST_ACT #$WERCKER_SLACK_POST_WURL [$WERCKER_SLACK_POST_GIT] by $WERCKER_STARTED_BY $WERCKER_RESULT."
+export WERCKER_SLACK_FALLBACK_MESSAGE="$WERCKER_APPLICATION_NAME:" \
+  " $WERCKER_SLACK_POST_ACT #${WERCKER_DEPLOY_ID:0:5}" \
+  " [$WERCKER_GIT_REPOSITORY/$WERCKER_GIT_BRANCH@${WERCKER_GIT_COMMIT:0:3}]" \
+  " by $WERCKER_STARTED_BY $WERCKER_RESULT."
 
-json="{\"text\":\"$WERCKER_SLACK_POST_MESSAGE\",\"channel\":\"#$WERCKER_SLACK_POST_CHANNEL\",\"icon_url\":\"https://2.gravatar.com/avatar/f777ecfdf484eed89dc6f215b78fef11?d=57\",\"username\":\"$WERCKER_SLACK_POST_USERNAME\"}"
+export MESSAGE_TEXT="$WERCKER_SLACK_POST_ACT $WERCKER_SLACK_POST_WURL" \
+  " $WERCKER_RESULT in $WERCKER_TIME_SPENT" \
+
+case "$WERCKER_RESULT" in
+  "passed") export MESSAGE_COLOR="#36A64F" ;;
+  "failed") export MESSAGE_COLOR="#A63636" ;;
+  *)        export MESSAGE_COLOR="#3663A6" ;;
+esac
+
+# "channel":  "#$WERCKER_SLACK_POST_CHANNEL",
+json=<<END
+{
+  "fallback": "$WERCKER_SLACK_FALLBACK_MESSAGE",
+  "channel":  "#test",
+  "icon_url": "https://2.gravatar.com/avatar/f777ecfdf484eed89dc6f215b78fef11?d=57",
+  "username": "$WERCKER_SLACK_POST_USERNAME",
+  "attachments": [{
+    "fallback":    "Build $WERCKER_RESULT in $WERCKER_TIME_SPENT",
+    "color":       "$MESSAGE_COLOR",
+    "author_name": "$WERCKER_STARTED_BY",
+    "title":       "$WERCKER_APPLICATION_NAME",
+    "title_link":  "$WERCKER_APPLICATION_URL",
+    "text":        "$MESSAGE_TEXT",
+    "fields": [
+        {
+            "title": "Commit",
+            "value": "$WERCKER_SLACK_POST_GIT",
+            "short": true
+        },
+        {
+            "title": "Branch",
+            "value": "$WERCKER_GIT_BRANCH",
+            "short": true
+        }
+    ],
+    "image_url": "http://my-website.com/path/to/image.jpg",
+    "thumb_url": "http://example.com/path/to/thumb.png"
+  }]
+}
+END
 
 RESULT=`curl -d "payload=$json" -s  "$WERCKER_SLACK_POST_URL" --output $WERCKER_STEP_TEMP/result.txt -w "%{http_code}"`
 
@@ -58,3 +108,6 @@ fi
 if [ "$RESULT" = "404" ]; then
   fail "Subdomain or token not found."
 fi
+
+
+# https://thevoidray.slack.com/archives/flux/p1456943630000106
