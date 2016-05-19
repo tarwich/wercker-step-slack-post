bb9a42a766b091ccde4064a23ec4adf1b06ee151
Samuel Dillow <tarwich@gmail.com>
2016-05-19 16:15:30 -0500

    Prettied up the message

--- run.sh
+++ run.sh
@@ -20,11 +20,11 @@ if [[ $WERCKER_SLACK_POST_CHANNEL == \#* ]]; then
 fi
 
 if [ ! -n "$DEPLOY" ]; then
-  export WERCKER_SLACK_POST_ACT="build"
-  export WERCKER_SLACK_POST_WURL="<$WERCKER_BUILD_URL|${WERCKER_BUILD_ID:0:5}...>"
+  export WERCKER_SLACK_POST_ACT="Build"
+  export WERCKER_SLACK_POST_WURL="#<$WERCKER_BUILD_URL|${WERCKER_BUILD_ID:0:7}>"
 else
-  export WERCKER_SLACK_POST_ACT="deploy"
-  export WERCKER_SLACK_POST_WURL="<$WERCKER_DEPLOY_URL|${WERCKER_DEPLOY_ID:0:5}...>"
+  export WERCKER_SLACK_POST_ACT="Deploy"
+  export WERCKER_SLACK_POST_WURL="#<$WERCKER_DEPLOY_URL|${WERCKER_DEPLOY_ID:0:7}>"
 fi
 
 if [[ $WERCKER_GIT_DOMAIN == bitbucket* ]]; then export GIT_TREE="commits"; else export GIT_TREE="commit"; fi
@@ -45,7 +45,7 @@ WERCKER_SLACK_FALLBACK_MESSAGE="$WERCKER_APPLICATION_NAME:\
  by $WERCKER_STARTED_BY $WERCKER_RESULT."
 
 export MESSAGE_TEXT="$WERCKER_SLACK_POST_ACT $WERCKER_SLACK_POST_WURL\
- $WERCKER_RESULT in $WERCKER_TIME_SPENT"
+ _${WERCKER_RESULT}_ in $WERCKER_TIME_SPENT"
 
 case "$WERCKER_RESULT" in
   "passed") export MESSAGE_COLOR="#36A64F" ;;
@@ -53,11 +53,10 @@ case "$WERCKER_RESULT" in
   *)        export MESSAGE_COLOR="#3663A6" ;;
 esac
 
-# "channel":  "#$WERCKER_SLACK_POST_CHANNEL",
 json=$(cat <<END
 {
   "fallback": "$WERCKER_SLACK_FALLBACK_MESSAGE",
-  "channel":  "#test",
+  "channel":  "#$WERCKER_SLACK_POST_CHANNEL",
   "icon_url": "https://2.gravatar.com/avatar/f777ecfdf484eed89dc6f215b78fef11?d=57",
   "username": "$WERCKER_SLACK_POST_USERNAME",
   "attachments": [{
@@ -83,7 +82,8 @@ json=$(cat <<END
     "thumb_url": "http://example.com/path/to/thumb.png"
   }]
 }
-END)
+END
+)
 
 RESULT=`curl -d "payload=$json" -s  "$WERCKER_SLACK_POST_URL" --output $WERCKER_STEP_TEMP/result.txt -w "%{http_code}"`
 
@@ -106,5 +106,3 @@ if [ "$RESULT" = "404" ]; then
 fi
 
 echo "Result:  $RESULT"
-
-# https://thevoidray.slack.com/archives/flux/p1456943630000106
